866875902b2fd60ec0109c3d87f0a9327645cb68
che <cheterz32@gmail.com>
2019-05-13 08:13:54 +0300

    add spell class

---
+++ app/src/main/java/com/cheterz/spellbook/Spell.java
@@ -0,0 +1,89 @@
+package com.cheterz.spellbook;
+
+public class Spell {
+
+    private String nameOfSpell;
+    private String levelOfSpell;
+    private String schoolOfSpell;
+    private String distanceOfSpell;
+    private String componentsOfSpell;
+    private String durationOfSpell;
+    private String classOfSpell;
+    private String sourceOfSpell;
+    private String descriptionOfSpell;
+
+
+
+    public String getNameOfSpell() {
+        return nameOfSpell;
+    }
+
+    public void setNameOfSpell(String nameOfSpell) {
+        this.nameOfSpell = nameOfSpell;
+    }
+
+    public String getLevelOfSpell() {
+        return levelOfSpell;
+    }
+
+    public void setLevelOfSpell(String levelOfSpell) {
+        this.levelOfSpell = levelOfSpell;
+    }
+
+    public String getSchoolOfSpell() {
+        return schoolOfSpell;
+    }
+
+    public void setSchoolOfSpell(String schoolOfSpell) {
+        this.schoolOfSpell = schoolOfSpell;
+    }
+
+    public String getDistanceOfSpell() {
+        return distanceOfSpell;
+    }
+
+    public void setDistanceOfSpell(String distanceOfSpell) {
+        this.distanceOfSpell = distanceOfSpell;
+    }
+
+    public String getComponentsOfSpell() {
+        return componentsOfSpell;
+    }
+
+    public void setComponentsOfSpell(String componentsOfSpell) {
+        this.componentsOfSpell = componentsOfSpell;
+    }
+
+    public String getDurationOfSpell() {
+        return durationOfSpell;
+    }
+
+    public void setDurationOfSpell(String durationOfSpell) {
+        this.durationOfSpell = durationOfSpell;
+    }
+
+    public String getClassOfSpell() {
+        return classOfSpell;
+    }
+
+    public void setClassOfSpell(String classOfSpell) {
+        this.classOfSpell = classOfSpell;
+    }
+
+    public String getSourceOfSpell() {
+        return sourceOfSpell;
+    }
+
+    public void setSourceOfSpell(String sourceOfSpell) {
+        this.sourceOfSpell = sourceOfSpell;
+    }
+
+    public String getDescriptionOfSpell() {
+        return descriptionOfSpell;
+    }
+
+    public void setDescriptionOfSpell(String descriptionOfSpell) {
+        this.descriptionOfSpell = descriptionOfSpell;
+    }
+
+}
