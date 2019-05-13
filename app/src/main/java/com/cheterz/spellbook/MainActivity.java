d9546e2cdc5208e6ac20ef2c901655aafb3f77df
che <cheterz32@gmail.com>
2019-05-13 15:02:26 +0300

    scrollable descr

--- app/src/main/java/com/cheterz/spellbook/MainActivity.java
+++ app/src/main/java/com/cheterz/spellbook/MainActivity.java
@@ -4,14 +4,18 @@ import android.app.FragmentTransaction;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.spell_card_detail);
+        TextView descr = findViewById(R.id.tv_text_of_description_detail);
+        descr.setMovementMethod(new ScrollingMovementMethod());
         Log.d("test", "start activity");
     }
 }
