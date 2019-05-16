bdb1df583e8ebd49fd8161662d94d25bd4da9200
che <cheterz32@gmail.com>
2019-05-16 16:27:26 +0300

    added rx&gson libs, add spell class and 2 json files

--- app/src/main/java/com/cheterz/spellbook/MainActivity.java
+++ app/src/main/java/com/cheterz/spellbook/MainActivity.java
@@ -4,14 +4,20 @@ import android.app.FragmentTransaction;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("test", "start activity");
+
     }
 }
--- app/src/main/java/com/cheterz/spellbook/Spell.java
+++ app/src/main/java/com/cheterz/spellbook/Spell.java
@@ -2,88 +2,14 @@ package com.cheterz.spellbook;
 
 public class Spell {
 
-    private String nameOfSpell;
-    private String levelOfSpell;
-    private String schoolOfSpell;
-    private String distanceOfSpell;
-    private String componentsOfSpell;
-    private String durationOfSpell;
-    private String classOfSpell;
-    private String sourceOfSpell;
-    private String descriptionOfSpell;
-
-
-
-    public String getNameOfSpell() {
-        return nameOfSpell;
-    }
-
-    public void setNameOfSpell(String nameOfSpell) {
-        this.nameOfSpell = nameOfSpell;
-    }
-
-    public String getLevelOfSpell() {
-        return levelOfSpell;
-    }
-
-    public void setLevelOfSpell(String levelOfSpell) {
-        this.levelOfSpell = levelOfSpell;
-    }
-
-    public String getSchoolOfSpell() {
-        return schoolOfSpell;
-    }
-
-    public void setSchoolOfSpell(String schoolOfSpell) {
-        this.schoolOfSpell = schoolOfSpell;
-    }
-
-    public String getDistanceOfSpell() {
-        return distanceOfSpell;
-    }
-
-    public void setDistanceOfSpell(String distanceOfSpell) {
-        this.distanceOfSpell = distanceOfSpell;
-    }
-
-    public String getComponentsOfSpell() {
-        return componentsOfSpell;
-    }
-
-    public void setComponentsOfSpell(String componentsOfSpell) {
-        this.componentsOfSpell = componentsOfSpell;
-    }
-
-    public String getDurationOfSpell() {
-        return durationOfSpell;
-    }
-
-    public void setDurationOfSpell(String durationOfSpell) {
-        this.durationOfSpell = durationOfSpell;
-    }
-
-    public String getClassOfSpell() {
-        return classOfSpell;
-    }
-
-    public void setClassOfSpell(String classOfSpell) {
-        this.classOfSpell = classOfSpell;
-    }
-
-    public String getSourceOfSpell() {
-        return sourceOfSpell;
-    }
-
-    public void setSourceOfSpell(String sourceOfSpell) {
-        this.sourceOfSpell = sourceOfSpell;
-    }
-
-    public String getDescriptionOfSpell() {
-        return descriptionOfSpell;
-    }
-
-    public void setDescriptionOfSpell(String descriptionOfSpell) {
-        this.descriptionOfSpell = descriptionOfSpell;
-    }
+    public static final String nameOfSpell = "";
+    public static final String levelOfSpell = "";
+    public static final String schoolOfSpell = "";
+    public static final String distanceOfSpell = "";
+    public static final String componentsOfSpell = "";
+    public static final String durationOfSpell = "";
+    public static final String classOfSpell = "";
+    public static final String sourceOfSpell = "";
+    public static final String descriptionOfSpell = "";
 
 }
