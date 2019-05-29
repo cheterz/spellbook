f5a498952432fc08d1f01b886f8393054ceae3c6
che <cheterz32@gmail.com>
2019-05-29 09:15:57 +0300

    Time to Kotlin, BIATCH

---
+++ app/src/main/java/com/cheterz/spellbook/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.cheterz.spellbook
+
+import android.app.FragmentTransaction
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("test", "start activity")
+
+    }
+}
---
+++ app/src/main/java/com/cheterz/spellbook/Spell.kt
@@ -0,0 +1,15 @@
+package com.cheterz.spellbook
+
+object Spell {
+
+    val nameOfSpell = ""
+    val levelOfSpell = ""
+    val schoolOfSpell = ""
+    val distanceOfSpell = ""
+    val componentsOfSpell = ""
+    val durationOfSpell = ""
+    val classOfSpell = ""
+    val sourceOfSpell = ""
+    val descriptionOfSpell = ""
+
+}
